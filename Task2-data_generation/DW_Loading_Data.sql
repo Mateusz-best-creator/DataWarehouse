9165aa110b167b3d12e849498e01456597b7a488
Mateusz <szachmat2004@gmail.com>
2025-03-20 17:01:10 +0100

    Generating all CSV files and preparing BULK inserts for SQL

--- Task2-data_generation/DW_Loading_Data.sql
+++ Task2-data_generation/DW_Loading_Data.sql
@@ -1,10 +1,60 @@
 use DateWarehouses_University;
 
 BULK INSERT Specialization
-		FROM 'C:/Users/User/Desktop/specialization.csv'
+		FROM 'C:/Users/User/Desktop/DataWarehouses/GeneratedCSV/specialization.csv'
 		WITH (
 			FIELDTERMINATOR = ',',
 			ROWTERMINATOR = '\n'
 		);
 
-select * from Specialization;
+BULK INSERT SpecializationMemberAssociation
+		FROM 'C:/Users/User/Desktop/DataWarehouses/GeneratedCSV/specialization_faculty_member.csv'
+		WITH (
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n'
+		);
+
+BULK INSERT FacultyMember
+		FROM 'C:/Users/User/Desktop/DataWarehouses/GeneratedCSV/faculty_member.csv'
+		WITH (
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n'
+		);
+
+BULK INSERT Student
+		FROM 'C:/Users/User/Desktop/DataWarehouses/GeneratedCSV/student.csv'
+		WITH (
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n'
+		);
+
+BULK INSERT Professor
+		FROM 'C:/Users/User/Desktop/DataWarehouses/GeneratedCSV/professor.csv'
+		WITH (
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n'
+		);
+
+BULK INSERT Department
+		FROM 'C:/Users/User/Desktop/DataWarehouses/GeneratedCSV/department.csv'
+		WITH (
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n'
+		);
+
+
+BULK INSERT Professor_Department
+		FROM 'C:/Users/User/Desktop/DataWarehouses/GeneratedCSV/professor_department.csv'
+		WITH (
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n'
+		);
+
+BULK INSERT Student_Department
+		FROM 'C:/Users/User/Desktop/DataWarehouses/GeneratedCSV/student_department.csv'
+		WITH (
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n'
+		);
+
+select * from Student_Department;
